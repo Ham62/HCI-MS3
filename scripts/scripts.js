06954fe2db0641833465d5403602fcdb9675cc83
Graham Downey <grahamdowney1@gmail.com>
2021-11-26 12:03:50 -0600

    Settings page

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -4,6 +4,8 @@ var orderList = [];
 // Which range did we select for the graph?
 var selectedRange = "week";
 
+var budgetGoal = 0;
+
 function createID() {
 	var result           = '';
 	var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
@@ -35,6 +37,162 @@ function createBubble(innerContent) {
 	return bubble;
 }
 
+function showSettings() {
+	var docContent = document.getElementById("content");
+	docContent.innerHTML = "";
+
+	var setGoal = '<FORM ID="setBudgetGoal">' +
+			'<H2>Budget Goals</H2>' +
+			'<LABEL FOR="budgetInput">Weekly spending limit: $</LABEL>' +
+			'<INPUT ID="budgetInput" NAME="budgetInput" TYPE="number" min="0" step="0.01" placeholder="' + budgetGoal.toFixed(2) + '">' +
+			'</FORM>' +
+			'<A HREF="javascript:void(0)" onClick="setNewBudget()">Set New Budget</A>';
+
+	docContent.appendChild(createBubble(setGoal));
+}
+
+function setNewBudget() {
+	var formData = new FormData(document.getElementById("setBudgetGoal"));
+
+	var budgetInput = formData.get("budgetInput");
+	if (budgetInput == "") {
+		alert("No changes made...");
+	} else {
+		budgetGoal = parseFloat(budgetInput);
+		alert("Changes saved!");
+	}
+
+}
+
+
+function showHome() {
+	// Clear document container
+	var docContent = document.getElementById("content");
+	docContent.innerHTML = "";
+
+	// Create "Add order" bubble
+	var innerContent =  '<DIV class="addOrder">' +
+				'<A HREF="javascript:void(0);" onclick="showAddOrderMenu()">' +
+				'<IMG SRC="media/images/addbutton.png">Add Order...</A>' +
+				'</DIV>';
+	var addOrderBtn = createBubble(innerContent);
+
+	docContent.appendChild(addOrderBtn);
+
+	// Append order bubbles to document container
+	for (i = 0; i < orderList.length; i++) {
+		docContent.appendChild(orderList[i]);
+	}
+
+	if (orderList.length == 0) {
+		var noOrders = document.createElement("DIV");
+		noOrders.setAttribute("style", "text-align: center; font-size: 27pt; color: #D0D0D0; margin-top: 2em;");
+		noOrders.innerHTML = "No Orders Yet...";
+		docContent.appendChild(noOrders);
+	}
+}
+
+/////////////////////////////////////
+//         Add order menu          //
+//           functions             //
+/////////////////////////////////////
+
+// Show order menu
+function showAddOrderMenu() {
+	// Clear document container
+	var docContent = document.getElementById("content");
+	docContent.innerHTML = "";
+
+	var innerContent = '<DIV class="center">' +
+			'<FORM ID="newOrderForm">' +
+				'<H2>Add Order</H2>' +
+				'<LABEL FOR="mealType">Meal Type:</LABEL><BR>' +
+				'<SELECT NAME="mealType" ID="mealType">' +
+					'<OPTION VALUE="none">Pick One...</OPTION>' +
+					'<OPTION VALUE="Breakfast">Breakfast</OPTION>' +
+					'<OPTION VALUE="Lunch">Lunch</OPTION>' +
+					'<OPTION VALUE="Dinner">Dinner</OPTION>' +
+					'<OPTION VALUE="Snack">Snack/Other</OPTION>' +
+				'</SELECT><BR>' +
+				'<LABEL FOR="locationSel">Where did you order from?</LABEL><BR>' +
+				'<INPUT TYPE="text" ID="locationSel" NAME="locationSel"><BR>' +
+				'<LABEL FOR="appSel">Select App</LABEL><BR>' +
+				'<SELECT NAME="appSel" ID="locationSel">' +
+					'<OPTION VALUE="none">Pick One...</OPTION>' +
+					'<OPTION VALUE="Doordash">Doordash</OPTION>' +
+					'<OPTION VALUE="Skip The Dishes">Skip The Dishes</OPTION>' +
+					'<OPTION VALUE="Uber Eats">Uber Eats</OPTION>' +
+					'<OPTION VALUE="Other">Other</OPTION>' +
+				'</SELECT><BR>' +
+				'<LABEL FOR="orderName">What did you order?</LABEL><BR>' +
+				'<INPUT TYPE="text" ID="orderName" NAME="orderName"><BR>' +
+				'<LABEL FOR="orderCost">Cost: $</LABEL><BR>' +
+				'<INPUT TYPE="number" ID="orderCost" NAME="orderCost" min="0" step="0.01" placeholder="0.00"><BR>' +
+			'</FORM>' +
+			'<BUTTON id="btnConfirm" onclick="submitOrder()">Confirm</BUTTON><BUTTON id="btnCancel" onclick="showHome()">Cancel</BUTTON>' +
+			//'<A HREF="javascript:void(0)" class="confirm" onclick="submitOrder()">Confirm</A>' +
+			//'<A HREF="javascript:void(0)" class="cancel" onclick="showHome()">Cancel</A>' +
+			'</DIV>';
+
+
+	var addOrderMenu = createBubble(innerContent);
+	docContent.appendChild(addOrderMenu);
+}
+
+// Create new order bubble and add to list
+function addOrderBubble(appSel, locationSel, mealType, mealName, mealCost) {
+	var innerContent = 
+				'<DIV class="orderSource">' + locationSel + ' - ' + mealType + '</DIV>' +
+				'<DIV class="orderType">' + mealType + '</DIV>' +
+				'<DIV class="orderDate">' + new Date().toLocaleDateString() + '</DIV><BR>' +
+				'<DIV class="orderName">' + mealName + " - $" + mealCost + '</DIV>' +
+				'<DIV class="orderApp">' + appSel + '</DIV>';
+
+	var orderBubble = createBubble(innerContent);
+	orderList.unshift(orderBubble); // Place at top of array
+
+	// Go to home page (order list)
+	showHome();
+}
+
+// Submit new order
+function submitOrder() {
+	var formData = new FormData(document.getElementById("newOrderForm"));
+
+	var mealType = formData.get("mealType");
+	if (mealType == "none") {
+		alert("Please select a meal type...");
+		return;
+	}
+
+	var locationSel = formData.get("locationSel");
+	if (locationSel == "") {
+		alert("Please enter where you ordered from...");
+		return;
+	}
+
+	var appSel = formData.get("appSel");
+	if (appSel == "none") {
+		alert("Please select an order app...");
+		return;
+	}
+
+	var mealName = formData.get("orderName").trim();
+	if (mealName == "") {
+		alert("Please enter the meal name...");
+		return;
+	}
+
+	var mealCost = formData.get("orderCost").trim();
+	if (mealCost == "") {
+		alert("Please enter the meal cost...");
+		return;
+	}
+
+	addOrderBubble(appSel, locationSel, mealType, mealName, mealCost);
+}
+
+
 function showStats() {
 	var docContent = document.getElementById("content");
 	docContent.innerHTML = "";
@@ -48,7 +206,7 @@ function showStats() {
 				'<INPUT TYPE = "radio" ID="week" name="rangeSel" value="week" onClick="changeRangeSel(this)">' +
 				'<LABEL FOR="week">Last Week</LABEL>' +
 				'</FORM>' +
-				'<A HREF="">Change Budget Goals</A>';
+				'<A HREF="javascript:void(0)" onClick="showSettings()">Change Budget Goals</A>';
 
 	var bblSelRange = createBubble(selectRange);
 	docContent.appendChild(bblSelRange);
@@ -69,11 +227,18 @@ function changeRangeSel(newSelection) {
 function genGraphs() {
 	var docContent = document.getElementById("content");
 
+	// Spending over time bar chart
 	docContent.appendChild(createBubble('<canvas id="spendingChart"></canvas>'));
 	addSpendingGraph();
 
+	// Budget goal report
+	var budgetGoal = createBubble('<div id="spendgoals">'+addBudgetGoalBubble()+'</div>');
+	docContent.appendChild(budgetGoal);
+
+	// Order location pie chart
 	docContent.appendChild(createBubble('<canvas id="locationChart"></canvas>'));
 	addLocationChart();
+
 }
 
 function addSpendingGraph() {
@@ -154,10 +319,7 @@ function addSpendingGraph() {
 	                    'Week 1',
 	                    'Week 2',
 	                    'Week 3',
-	                    'Week 4',
-	                    'Week 5',
-	                    'Week 6',
-	                    'Week 7'
+	                    'Week 4'
 	                    ],
 	                datasets: [{
 	                    label: '',
@@ -353,131 +515,26 @@ function addLocationChart() {
 			}
 		});
 	}
-					
-	
 }
 
-
-function showHome() {
-	// Clear document container
-	var docContent = document.getElementById("content");
-	docContent.innerHTML = "";
-
-	// Create "Add order" bubble
-	var innerContent =  '<A HREF="javascript:void(0);" onclick="showAddOrderMenu()">' +
-				'<IMG SRC="media/images/addbutton.png">Add Order...</A>';
-	var addOrderBtn = createBubble(innerContent);
-
-	docContent.appendChild(addOrderBtn);
-
-	// Append order bubbles to document container
-	for (i = 0; i < orderList.length; i++) {
-		docContent.appendChild(orderList[i]);
-	}	
-}
-
-/////////////////////////////////////
-//         Add order menu          //
-//           functions             //
-/////////////////////////////////////
-
-// Show order menu
-function showAddOrderMenu() {
-	// Clear document container
-	var docContent = document.getElementById("content");
-	docContent.innerHTML = "";
-
-	var innerContent = '<DIV class="center">' +
-			'<FORM ID="newOrderForm">' +
-				'<H2>Add Order</H2>' +
-				'<LABEL FOR="mealType">Meal Type:</LABEL><BR>' +
-				'<SELECT NAME="mealType" ID="mealType">' +
-					'<OPTION VALUE="none">Pick One...</OPTION>' +
-					'<OPTION VALUE="Breakfast">Breakfast</OPTION>' +
-					'<OPTION VALUE="Lunch">Lunch</OPTION>' +
-					'<OPTION VALUE="Dinner">Dinner</OPTION>' +
-					'<OPTION VALUE="Snack">Snack/Other</OPTION>' +
-				'</SELECT><BR>' +
-				'<LABEL FOR="locationSel">Where did you order from?</LABEL><BR>' +
-				'<INPUT TYPE="text" ID="locationSel" NAME="locationSel"><BR>' +
-				'<LABEL FOR="appSel">Select App</LABEL><BR>' +
-				'<SELECT NAME="appSel" ID="locationSel">' +
-					'<OPTION VALUE="none">Pick One...</OPTION>' +
-					'<OPTION VALUE="Doordash">Doordash</OPTION>' +
-					'<OPTION VALUE="Skip The Dishes">Skip The Dishes</OPTION>' +
-					'<OPTION VALUE="Uber Eats">Uber Eats</OPTION>' +
-					'<OPTION VALUE="Other">Other</OPTION>' +
-				'</SELECT><BR>' +
-				'<LABEL FOR="orderName">What did you order?</LABEL><BR>' +
-				'<INPUT TYPE="text" ID="orderName" NAME="orderName"><BR>' +
-				'<LABEL FOR="orderCost">Cost: $</LABEL><BR>' +
-				'<INPUT TYPE="number" ID="orderCost" NAME="orderCost" min="0" step="0.01" placeholder="0.00"><BR>' +
-			'</FORM>' +
-			'<BUTTON onclick="submitOrder()">Confirm</BUTTON><BUTTON onclick="resetOrder()">Reset</BUTTON>' +
-			'</DIV>';
-
-
-	var addOrderMenu = createBubble(innerContent);
-	docContent.appendChild(addOrderMenu);
-}
-
-// Create new order bubble and add to list
-function addOrderBubble(appSel, locationSel, mealName, mealCost) {
-	var innerContent = '<DIV class="orderSource">' + locationSel + '</DIV>' +
-				'<DIV class="orderDate">' + new Date().toLocaleDateString() + '</DIV><BR>' +
-				'<DIV class="orderName">' + mealName + " - $" + mealCost + '</DIV>' +
-				'<DIV class="orderApp">' + appSel + '</DIV>';
-
-	var orderBubble = createBubble(innerContent);
-	orderList.unshift(orderBubble); // Place at top of array
-
-	// Go to home page (order list)
-	showHome();
-}
-
-// Reset form
-function resetOrder() {
-	var form = document.getElementById("newOrderForm");
-	var formData = new FormData(document.getElementById("newOrderForm"));
-
-	
-}
-
-// Submit new order
-function submitOrder() {
-	var formData = new FormData(document.getElementById("newOrderForm"));
-
-	var mealType = formData.get("mealType");
-	if (mealType == "none") {
-		alert("Please select a meal type...");
-		return;
+function addBudgetGoalBubble() {
+	var congrats, dollar, over, range;
+	if (selectedRange == "week") {
+		congrats = "Sorry, y";
+		dollar = "12";
+		over = "over";
+		range = "weekly";
+	} else if (selectedRange == "month") {
+		congrats = "Congrats! Y";
+		dollar = "6";
+		over = "under";
+		range = "monthly";
+	} else {
+		congrats = "Congrats! Y";
+		dollar = "43";
+		over = "under";
+		range = "yearly";
 	}
 
-	var locationSel = formData.get("locationSel");
-	if (locationSel == "") {
-		alert("Please enter where you ordered from...");
-		return;
-	}
-
-	var appSel = formData.get("appSel");
-	if (appSel == "none") {
-		alert("Please select an order app...");
-		return;
-	}
-
-	var mealName = formData.get("orderName").trim();
-	if (mealName == "") {
-		alert("Please enter the meal name...");
-		return;
-	}
-
-	var mealCost = formData.get("orderCost").trim();
-	if (mealCost == "") {
-		alert("Please enter the meal cost...");
-		return;
-	}
-
-	addOrderBubble(appSel, locationSel, mealName, mealCost);
+	return congrats + 'ou spent $' + dollar + ' ' + over + ' your ' + range + ' budget!';
 }
-
-
